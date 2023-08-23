8c7ccd6082446bde93fdad82b8da0d5521cb3155
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-23 10:48:57 +0300

    Add to pageContent in ProductList and add Product component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import useCart from "../hooks/useCart"
 import useProducts from "../hooks/useProducts"
 import { UseProductsContextType } from "../context/ProductsProvider"
 import { ReactElement } from "react"
+import Product from "./Product"
 
 const ProductList = () => {
   const { dispatch, REDUCER_ACTIONS, cart} = useCart()
@@ -9,9 +10,29 @@ const ProductList = () => {
 
   let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>
 
-  return (
-    <div></div>
+  if (products?.length) {
+    pageContent = products.map(product => {
+      const inCart: boolean = cart.some(item => item.sku === product.sku) //The .some() method in JavaScript and TypeScript always returns either true or false. It's used to determine whether at least one element in an array satisfies a given condition
+
+      return (
+        <Product
+          key={product.sku}
+          product={product}
+          dispatch={dispatch}
+          REDUCER_ACTIONS={REDUCER_ACTIONS}
+          inCart={inCart}
+        />
+      )
+    })
+  }
+
+  const content = (
+    <main className="main main--products">
+      {pageContent}
+    </main>
   )
+
+  return content
 }   
 
 export default ProductList
