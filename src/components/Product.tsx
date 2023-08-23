184aa8c314cc58adada7fd40aed1b0efa82c0388
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-23 14:23:36 +0300

    Add content for Product, Wrap App with ProductsProvider and CartProvider

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -18,9 +18,16 @@ const Product = ({ product, dispatch, REDUCER_ACTIONS, inCart }: PropsType): Rea
 
   const itemInCart = inCart ? ' → Item in Cart: ✔️' : null
 
-  return (
-    <div></div>
+  const content = (
+    <article className="product">
+      <h3>{product.name}</h3>
+      <img src={img} alt={product.name} className="product__img" />
+      <p>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}{itemInCart}</p>
+      <button onClick={onAddToCart}>Add to Cart</button>
+    </article>
   )
+
+  return content
 }
 
 export default Product
